c6d53f458c753165a96c938d86fefc42ef452f30
bienenweiss-lab <bienen.weiss@gmail.com>
2026-02-01 21:01:53 +0100

    node server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.enable("trust proxy");
 
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwcZo9e-uUUpYPfFedizuy9bdKdNQ-UpidLpTYA-KQQdfuFdtE/exec";
 
+// Test-Route für SIM800: zeigt, ob der Node-Server erreicht wird
+app.get("/", (req, res) => {
+    console.log("Root wurde aufgerufen");
+    res.send("OK ROOT");
+});
+
+// Haupt-Upload-Route
 app.get("/upload", async (req, res) => {
     console.log("Bienenwaage Upload:", req.query);
 
